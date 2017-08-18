1376c70ed15932f171c9cbae7431ff7d8591d54c
Oliver Borchert <oliver.borchert@in.tum.de>
2017-08-18 18:32:11 +0200

    Initial Commit

---
+++ CoreCache/CoreCache.h
@@ -0,0 +1,19 @@
+//
+//  CoreCache.h
+//  CoreCache
+//
+//  Created by Oliver Borchert on 8/18/17.
+//  Copyright © 2017 Oliver Borchert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreCache.
+FOUNDATION_EXPORT double CoreCacheVersionNumber;
+
+//! Project version string for CoreCache.
+FOUNDATION_EXPORT const unsigned char CoreCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreCache/PublicHeader.h>
+
+
